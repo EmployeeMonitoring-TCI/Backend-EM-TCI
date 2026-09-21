0be7480964ae404891cfe7a814f4bacfe9bbaf73
nsaaiisma <fiismatunnissa@gmail.com>
2026-09-21 09:53:51 +0700

    dblib

--- main.py
+++ main.py
@@ -38,4 +38,4 @@ app.include_router(otp.router)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run("main:app", host="127.0.0.1", port=12046, reload=True)
+    uvicorn.run("main:app", host="0.0.0.0", port=12046, reload=True)
